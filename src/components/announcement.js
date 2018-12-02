ac8707b079932a37a98c982430a8ca7bfbad55ea
Alexandre Rimthong <rimthong@alexterm.local>
2018-12-01 21:48:00 -0500

    Fixed some issues with first page

--- src/components/announcement.js
+++ src/components/announcement.js
@@ -17,16 +17,20 @@ const t = {
 
 const Announcement = ({lang}) => (
   <Screen id="home" image={homebg}>
-    <svg viewBox="0 0 200 100">
-      <path id="curve" d="M10 90 Q 100 20 190 90" />
-      <text width="100%"  text-align="justify" style={{fill: 'white', fontFamily: 'pacifico'}}>
-        <textPath href="#curve">
-          {t[lang].curveText}
-        </textPath>
-      </text>
-    </svg>
-    <h1>{t[lang].date1}</h1>
-    <h1>{t[lang].date2}</h1>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between', height: '100%', width: '100%', maxWidth: '600px'}}>
+      <svg viewBox="0 0 200 100">
+        <path id="curve" d="M10 90 Q 100 20 190 90" />
+        <text width="100%"  text-align="justify" style={{fill: 'white', fontFamily: 'pacifico'}}>
+          <textPath href="#curve">
+            {t[lang].curveText}
+          </textPath>
+        </text>
+      </svg>
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <h1>{t[lang].date1}</h1>
+        <h1>{t[lang].date2}</h1>
+      </div>
+    </div>
   </Screen>
 );
 
